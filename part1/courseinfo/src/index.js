552c3b277e9bf86f1eda6ae0aa4119ec62a53659
Anders B. Ladefoged <anders@bladefoged.com>
2020-04-07 12:08:21 +0200

    Exercise 1.5 completed

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -19,7 +19,6 @@ const Part = (props) => {
 }
 
 const Content = (props) => {
-    console.log(props.parts[0])
     return (
         <>
           <Part part={props.parts[0]} />
@@ -39,27 +38,29 @@ const Total = (props) => {
 
 
 const App = () => {
-    const course = 'Half Stack application development'
+    const course = {
+        name: 'Half Stack application development',
     
-    const parts  = [
-        {
-            name: 'Fundamentals of React',
-            exercises: 10
-        },
-        {
-            name: 'Using props to pass data',
-            exercises: 7
-        },
-        {
-            name: 'State of a component',
-            exercises: 14
-        }]
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }]
+    }
     
     return (
         <div>
-          <Header course={course}/>
-          <Content parts={parts} />
-          <Total parts={parts}/>
+          <Header course={course.name}/>
+          <Content parts={course.parts} />
+          <Total parts={course.parts}/>
         </div>					
     )
 }
