9ea2fb6cff88619b59656d8630abf991dfc749a0
Anders B. Ladefoged <anders@bladefoged.com>
2020-04-07 16:15:43 +0200

    Exercise 1.8 completed

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -10,6 +10,21 @@ const Button = ({ onClick, text }) => (
   </button>
 )
 
+const Statistics = ({good, neutral, bad}) => {
+    const average = Math.round((good - bad) / (good + neutral + bad) * 100 + Number.EPSILON) / 100
+    const positive = Math.round((good + neutral) / (good + neutral + bad) * 10000) / 100
+
+    return (
+        <>
+          <p>Good: {good}</p>
+          <p>Neutral: {neutral}</p>
+          <p>Bad: {bad}</p>
+          <p>Average: {average}</p>
+          <p>Positive: {positive}%</p>
+		</>
+	)
+}
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
@@ -23,11 +38,7 @@ const App = () => {
         <Button onClick={() => setNeutral(neutral + 1)} text='Neutral' />
         <Button onClick={() => setBad(bad + 1)} text='Bad' />
         <Header text='Statistics'/>
-        <p>Good: {good}</p>
-        <p>Neutral: {neutral}</p>
-        <p>Bad: {bad}</p>
-        <p>Average: {(good - bad) / (good + neutral + bad)}</p>
-        <p>Positive: {(good + neutral) / (good + neutral + bad) * 100}%</p>
+        <Statistics good={good} neutral={neutral} bad={bad} />
       </div>
   )
 }
