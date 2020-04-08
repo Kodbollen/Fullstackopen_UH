1993c688c6ab18c98859e4d00b94564b603499d4
Anders B. Ladefoged <anders@bladefoged.com>
2020-04-08 12:47:10 +0200

    Exercise 2.2 and 2.3 completed

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Note from './components/Note'
 import Course from './components/Course'
 
 const App = () => {
@@ -7,21 +6,26 @@ const App = () => {
     id: 1,
     name: 'Half Stack application development',
     parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
+        {
+            name: 'Fundamentals of React',
+            exercises: 10,
+            id: 1
+        },
+        {
+            name: 'Using props to pass data',
+            exercises: 7,
+            id: 2
+        },
+        {
+            name: 'State of a component',
+            exercises: 14,
+            id: 3
+        },
+        {
+            name: 'Redux',
+            exercises: 11,
+            id: 4
+        }
     ]
   }
 
--- part2/notes/src/components/Course.js
+++ part2/notes/src/components/Course.js
@@ -7,7 +7,11 @@ const Part = ({name, exercises}) => (<p>{name} {exercises}</p>)
 const Content = ({parts}) => (
     <div>
       {parts.map(part => <Part key={part.id} name={part.name} exercises={part.exercises} />)}
+      {console.log(parts[0].exercises)}
+      <b>Total number of exercises: {parts.map(obj => obj.exercises).reduce((a, b) => a + b)}</b>
 	</div>)
+
+
 
 const Course = ({course}) => (
     <div>
