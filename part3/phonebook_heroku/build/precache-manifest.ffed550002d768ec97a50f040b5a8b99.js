58307985f9422db2384db48d4dd267d8ad65cd4d
Anders B. Ladefoged <anders@bladefoged.com>
2020-04-14 00:00:55 +0200

    recommit after removing .git from heroku repo

---
+++ part3/phonebook_heroku/build/precache-manifest.ffed550002d768ec97a50f040b5a8b99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c797b42bbd87d2fb8d66d369b8cee55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7495e5edc66733bb904c",
+    "url": "/static/css/main.5cbe915c.chunk.css"
+  },
+  {
+    "revision": "5bb0fd0074a4317e3eb6",
+    "url": "/static/js/2.86fc60a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.86fc60a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7495e5edc66733bb904c",
+    "url": "/static/js/main.8b2d50eb.chunk.js"
+  },
+  {
+    "revision": "d19032c1263fa01ea41a",
+    "url": "/static/js/runtime-main.08633c47.js"
+  }
+]);
