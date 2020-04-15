36b522e93538694182e33871ba648ff0ab7acf66
Anders B. Ladefoged <anders@bladefoged.com>
2020-04-16 01:49:21 +0200

    Exercise 4.5 completed

--- part4/blog_list/tests/list_helper.test.js
+++ part4/blog_list/tests/list_helper.test.js
@@ -31,3 +31,38 @@ describe('Total upvotes', () => {
 		expect(total).toBe(36)
 	})
 })
+
+describe('Favourite blog', () => {
+	const blogs = [
+		{ _id: "5a422a851b54a676234d17f7", title: "React patterns", author: "Michael Chan", url: "https://reactpatterns.com/", upvotes: 7, __v: 0 },
+		{ _id: "5a422aa71b54a676234d17f8", title: "Go To Statement Considered Harmful", author: "Edsger W. Dijkstra", url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html", upvotes: 5, __v: 0 },
+		{ _id: "5a422b3a1b54a676234d17f9", title: "Canonical string reduction", author: "Edsger W. Dijkstra", url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html", upvotes: 12, __v: 0 },
+		{ _id: "5a422b891b54a676234d17fa", title: "First class tests", author: "Robert C. Martin", url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll", upvotes: 10, __v: 0 },
+		{ _id: "5a422ba71b54a676234d17fb", title: "TDD harms architecture", author: "Robert C. Martin", url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html", upvotes: 0, __v: 0 },
+		{ _id: "5a422bc61b54a676234d17fc", title: "Type wars", author: "Robert C. Martin", url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html", upvotes: 2, __v: 0 }]
+
+	const singleBlog = [{ _id: "5a422bc61b54a676234d17fc", title: "Type wars", author: "Robert C. Martin", url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html", upvotes: 2, __v: 0 }]
+	const transform = (blog) => {
+		return {
+			title: blog.title,
+			author: blog.author,
+			upvotes: blog.upvotes
+		}
+	}
+	test('empty list should return empty object', () => {
+		const favourite = listHelper.favouriteBlog([])
+		expect(favourite).toEqual({})
+	})
+	test('list of one object should return that object', () => {
+		const favourite = listHelper.favouriteBlog(singleBlog)
+		const expectedObject = transform(singleBlog[0])
+		expect(favourite).toEqual(expectedObject)
+	})
+	test('longer list should result in correct total', () => {
+		const favourite = listHelper.favouriteBlog(blogs)
+		const expectedObject = transform(blogs[2])
+		expect(favourite).toEqual(expectedObject)		
+	})
+})
+
+
--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -6,6 +6,20 @@ const totalUpvotes = (blogs) => {
 	return blogs.map(blog => blog.upvotes).reduce((acc, val) => acc + val, 0)
 }
 
+const favouriteBlog = (blogs) => {
+	if (blogs.length === 0) return {}
+	const index = blogs.map(blog => blog.upvotes).indexOf(Math.max(...blogs.map(blog => blog.upvotes)))
+	const favourite = blogs[index]
+	const returnObject = {
+		title: favourite.title,
+		author: favourite.author,
+		upvotes: favourite.upvotes
+	}
+	return returnObject
+}
+
 module.exports = {
-	dummy, totalUpvotes
+	dummy,
+	totalUpvotes,
+	favouriteBlog
 }
