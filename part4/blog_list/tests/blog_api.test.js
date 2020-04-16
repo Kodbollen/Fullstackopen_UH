ca443978717f870d19cdf674fd25f7a442d3e1a6
Anders B. Ladefoged <anders@bladefoged.com>
2020-04-16 14:03:03 +0200

    Exercise 4.8 fully completed

--- part4/blog_list/tests/blog_api.test.js
+++ part4/blog_list/tests/blog_api.test.js
@@ -3,6 +3,41 @@ const supertest = require('supertest')
 const app = require('../app')
 
 const api = supertest(app)
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+	{
+		title: "Blogs are bad",
+		author: "Anders",
+		url: "dinmor.com",
+		upvotes: 123,
+		id: "5e96ed64386aab14578c5bd8"
+	},
+	{
+		title: "Blogs are still bad",
+		author: "Anders",
+		url: "dinstadigemor.com",
+		upvotes: 1234,
+		id: "5e96fb2fa109141a915a6047"
+	},
+	{
+		title: "Blogs are unnecessary",
+		author: "Anders",
+		url: "dinligegyldigemor.com",
+		upvotes: 12345,
+		id: "5e9839ebd664500ec33f55f3"
+	}]
+
+beforeEach(async () => {
+	await Blog.deleteMany({})
+
+	let blogObject = new Blog(initialBlogs[0])
+	await blogObject.save()
+	blogObject = new Blog(initialBlogs[1])
+	await blogObject.save()
+	blogObject = new Blog(initialBlogs[2])
+	await blogObject.save()
+})
 
 test('blogs are returned as json', async () => {
 	const response = await api.get('/api/blogs')
@@ -10,14 +45,14 @@ test('blogs are returned as json', async () => {
 	expect(response.type).toBe('application/json')
 })
 
-test('There are three notes', async() => {
+test('All blogs are returned', async() => {
 	const response = await api.get('/api/blogs')
-	expect(response.body).toHaveLength(3)
+	expect(response.body).toHaveLength(initialBlogs.length)
 })
 
-test('First blog have correct title', async() => {
+test('Specific blog is within returned blogs', async() => {
 	const response = await api.get('/api/blogs')
-	expect(response.body[0].title).toBe('Blogs are shit')
+	expect(response.body.map(content => content.title)).toContain('Blogs are bad')
 })
 
 afterAll(() => {
