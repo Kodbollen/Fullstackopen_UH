6d97c0c6ebbab7a4bbfd03a1b0fc408155cf49ee
Anders B. Ladefoged <anders@bladefoged.com>
2020-04-24 15:11:43 +0200

    Exercise 5.16 completed

--- part5/bloglist-frontend/src/components/NewBlog.js
+++ part5/bloglist-frontend/src/components/NewBlog.js
@@ -18,13 +18,13 @@ const NewBlog = ({addBlog}) => {
             <div>
                 <h1>Create new blog</h1>
                 <div>
-                  title:<input type='text' value={title} name='title' onChange={({target}) => setTitle(target.value)}/>
+                  title:<input id='title' type='text' value={title} name='title' onChange={({target}) => setTitle(target.value)}/>
                 </div>
                 <div>
-                    author:<input type='text' value={author} name='author' onChange={({target}) => setAuthor(target.value)}/>
+                    author:<input id='author' type='text' value={author} name='author' onChange={({target}) => setAuthor(target.value)}/>
                 </div>
                 <div>
-                    url:<input type='text' value={url} name='url' onChange={({target}) => setUrl(target.value)}/>
+                    url:<input id='url' type='text' value={url} name='url' onChange={({target}) => setUrl(target.value)}/>
                 </div>
                 <button type='submit'>Create</button>
             </div>
