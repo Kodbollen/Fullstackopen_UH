c7d5e8e758d3a2182173fb046cd0e90028678555
Anders B. Ladefoged <anders@bladefoged.com>
2020-04-27 10:57:48 +0200

    Exercise 6.5 completed

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -5,6 +5,7 @@ import NewAnecdote from './components/NewAnecdote'
 
 const App = () => {
     const anecdotes = useSelector(state => state)
+    const anecdotesSortedByVotes = anecdotes.sort((a, b) => b.votes - a.votes)
     const dispatch = useDispatch()
 
     const vote = (id) => {
@@ -14,7 +15,7 @@ const App = () => {
     return (
         <div>
           <h2>Anecdotes</h2>
-          {anecdotes.map(anecdote =>
+          {anecdotesSortedByVotes.map(anecdote =>
                          <div key={anecdote.id}>
                            <div>
                              {anecdote.content}
