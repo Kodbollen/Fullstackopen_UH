68ec84102e735463106b67f66c5f3899a95a6f51
Anders B. Ladefoged <anders@bladefoged.com>
2020-04-30 23:27:37 +0200

    Properly added links to user page

--- part7/bloglist-frontend/src/pages/SingleUserPage.js
+++ part7/bloglist-frontend/src/pages/SingleUserPage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {useSelector} from 'react-redux'
-import {useParams} from 'react-router-dom'
+import {useParams, Link} from 'react-router-dom'
 
 const SingleUserPage = () => {
     const id = useParams().id
@@ -21,8 +21,8 @@ const SingleUserPage = () => {
           <ul>
             {user.blogs.map(blog => (
                 <li key={blog.id}>
-                  {/* <Link to={`/blogs/id`}>{blog.title} by {blog.author}</Link> */}
-                  {blog.title} by {blog.author}
+                  <Link to={`/blogs/${blog.id}`}>{blog.title}</Link>
+                  &nbsp; {blog.author}
                 </li>
             ))}
 		  </ul>
