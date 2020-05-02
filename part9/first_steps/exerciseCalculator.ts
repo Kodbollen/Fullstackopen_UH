e42cf29b909b48dd0691bcf739e72778c26221ea
Anders B. Ladefoged <anders@bladefoged.com>
2020-05-02 14:10:29 +0200

    Exercise 9.2 completed

---
+++ part9/first_steps/exerciseCalculator.ts
@@ -0,0 +1,42 @@
+interface TrainingReport {
+    periodLength: number,
+    trainingDays: number,
+    success: boolean,
+    rating: number,
+    ratingDescription: string,
+    target: number,
+    average: number
+}
+
+const calculateExercises = (history: Array<number>): TrainingReport => {
+    const target = 2
+    const periodLength = history.length
+    const trainingDays = history.filter(day => day > 0).length
+    const average = history.reduce((acc, val) => acc + val) / history.length
+    const success = average >= target
+    const getRating = (average: number, target: number): { rating: number, ratingDescription: string } => {
+        switch (true) {
+            case average >= target + 2:
+                return { rating: 3, ratingDescription: 'Brilliant!' }
+            case average >= target:
+                return { rating: 2, ratingDescription: 'Good work' }
+            default:
+                return { rating: 1, ratingDescription: 'Not good enough' }
+        }
+    }
+    const { rating, ratingDescription } = getRating(average, target)
+
+    return {
+        periodLength,
+        trainingDays,
+        success,
+        rating,
+        ratingDescription,
+        target,
+        average
+    }
+}
+
+
+
+
