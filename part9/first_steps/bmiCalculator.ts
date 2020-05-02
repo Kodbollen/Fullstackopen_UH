19e1aecd4d6cb6756e1d2a74988f1d5a366bfa7c
Anders B. Ladefoged <anders@bladefoged.com>
2020-05-02 21:56:42 +0200

    Exercise 9.3 completed

--- part9/first_steps/bmiCalculator.ts
+++ part9/first_steps/bmiCalculator.ts
@@ -1,27 +1,46 @@
-
-const calculateBMI = (height: number, weight: number): string => {
-	const bmi = weight / (height / 100)**2
-	switch(true) {
-		case bmi < 15:
-			return 'Very severely underweight'
-		case bmi < 16:
-			return 'Severely underweight'
-		case bmi < 18.5:
-			return 'Underweight'
-		case bmi < 25:
-			return 'Normal (Healthy weight)'
-		case bmi < 30:
-			return 'Overweight'
-		case bmi < 35:
-			return 'Obese class I (Moderately obese)'
-		case bmi < 40:
-			return 'Obese class II (Severely obese)'
-		case bmi >= 40:
-			return 'Obese class III (Very Severely obese)'
-		default:
-			return 'Error: BMI not classified'
-	}
+interface biometrics {
+    height: number,
+    weight: number
 }
 
-console.log(calculateBMI(180, 74))
+const parseArguments = (args: Array<string>): biometrics => {
+    if(args.length < 4) throw new Error('Not enough arguments')
+    if(args.length > 4) throw new Error('Too many arguments')
+    if(!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+        return {
+            height: Number(args[2]),
+            weight: Number(args[3])
+        }
+    }
+}
 
+const calculateBMI = (height: number, weight: number): string => {
+    const bmi = weight / (height / 100) ** 2
+    switch(true) {
+        case bmi < 15:
+            return 'Very severely underweight'
+        case bmi < 16:
+            return 'Severely underweight'
+        case bmi < 18.5:
+            return 'Underweight'
+        case bmi < 25:
+            return 'Normal (Healthy weight)'
+        case bmi < 30:
+            return 'Overweight'
+        case bmi < 35:
+            return 'Obese class I (Moderately obese)'
+        case bmi < 40:
+            return 'Obese class II (Severely obese)'
+        case bmi >= 40:
+            return 'Obese class III (Very Severely obese)'
+        default:
+            throw new Error('Error: BMI not classified')
+    }
+}
+
+try {
+    const {height, weight} = parseArguments(process.argv)
+    console.log(calculateBMI(height, weight))
+} catch(e) {
+    throw new Error(`Error: ${e.message}`)
+}
--- part9/first_steps/exerciseCalculator.ts
+++ part9/first_steps/exerciseCalculator.ts
@@ -8,23 +8,28 @@ interface TrainingReport {
     average: number
 }
 
+const getArgumentArray = (args: Array<string>): Array<number> => {
+    if(process.argv.length < 3) throw new Error('No arguments received')
+    return [...args].slice(2).map(n => Number(n))
+}
+
 const calculateExercises = (history: Array<number>): TrainingReport => {
     const target = 2
     const periodLength = history.length
     const trainingDays = history.filter(day => day > 0).length
     const average = history.reduce((acc, val) => acc + val) / history.length
     const success = average >= target
-    const getRating = (average: number, target: number): { rating: number, ratingDescription: string } => {
-        switch (true) {
+    const getRating = (average: number, target: number): {rating: number, ratingDescription: string} => {
+        switch(true) {
             case average >= target + 2:
-                return { rating: 3, ratingDescription: 'Brilliant!' }
+                return {rating: 3, ratingDescription: 'Brilliant!'}
             case average >= target:
-                return { rating: 2, ratingDescription: 'Good work' }
+                return {rating: 2, ratingDescription: 'Good work'}
             default:
-                return { rating: 1, ratingDescription: 'Not good enough' }
+                return {rating: 1, ratingDescription: 'Not good enough'}
         }
     }
-    const { rating, ratingDescription } = getRating(average, target)
+    const {rating, ratingDescription} = getRating(average, target)
 
     return {
         periodLength,
@@ -37,6 +42,5 @@ const calculateExercises = (history: Array<number>): TrainingReport => {
     }
 }
 
-
-
-
+const args = getArgumentArray(process.argv)
+console.log(calculateExercises(args))
